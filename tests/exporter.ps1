bf3fca38ed48038cf4f0a44c1cd77412b4532910
Klett IT GmbH <github@klett-it.net>
2025-05-21 14:27:19 +0200

    add integration test

---
+++ tests/exporter.ps1
@@ -0,0 +1,10 @@
+Import-Module "$PSScriptRoot/../PrometheusExporter"
+
+function Get-DummyMetrics {
+    $desc = New-MetricDescriptor -Name "dummy_metric" -Type gauge -Help "A dummy metric" -Labels @("source")
+    return New-Metric -MetricDesc $desc -Value 1 -Labels @("test")
+}
+
+$exp = New-PrometheusExporter -Port 9700
+Register-Collector -Exporter $exp -Collector ${function:Get-DummyMetrics}
+$exp.Start()
